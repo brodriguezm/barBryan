b8dba462787144615ea014892bb9189dde43f713
Bryan <bsteven1993@gmail.com>
2019-01-04 06:07:22 -0500

    Se agrego consulta de platillos en el home

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Platillo;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,10 +14,13 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $repository = $this->getDoctrine()->getRepository(Platillo::class);
+        $platillos = $repository->findAll();
+
         // replace this example code with whatever you need
-        return $this->render('bar/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('bar/index.html.twig', array(
+            "platillos" => $platillos
+        ));
     }
 
     /**
